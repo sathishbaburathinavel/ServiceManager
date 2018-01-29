48ad93820b801b76229713944477bd1b50565ecd
sathish <sathishbabur@hotmail.com>
2018-01-29 10:43:30 +0530

    Added the source

---
+++ Addon.ServiceManager/MyAuthorizationServerProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Addon.ServiceManager
+{
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task GrantClientCredentials(OAuthGrantClientCredentialsContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity=new Clai
+        }
+    }
+}
